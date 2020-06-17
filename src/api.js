baa9c029f6ea9fa6fb54ee6b005b975019aae18f
Rodolfo <brsrkdev@gmail.com>
2020-06-17 13:47:26 -0500

    Add api call

--- src/api.js
+++ src/api.js
@@ -1,5 +1,14 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL:
+    'https://api.outsidein.dev/UgGW9gOPBiC0jQ021UMPwAmiNEnxPYMm',
+});
+
 const api = {
-  loadRestaurants() { },
+  loadRestaurants() {
+    return client.get('/restaurants').then(response => response.data);
+  },
 };
 
 export default api;
